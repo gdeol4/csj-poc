6f806ebc190f464e43983f19f9eb16714fbc813b
Gurkamal Deol <gdeol4@uwo.ca>
2022-07-15 14:36:16 -0400

    Update Setting_up.py

--- pages/Setting_up.py
+++ pages/Setting_up.py
@@ -6,7 +6,7 @@ st.write('''The Anaconda package manager is a good choice for
         scientific computing and it's fast to get started with.''')
 st.markdown('''1. Download and install Anaconda with default 
            settings during installation: https://www.anaconda.com/products/distribution''')
-st.markdown('''2. Search for and open 'Anaconda prompt'.
+st.markdown('''2. Search for 'Anaconda prompt' on your computer and open it.
             ''')
 st.markdown('''3. Create an isolated development environment using the
             code below. Chose a name for the environment.
