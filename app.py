421dfbd319d9aa7c44890bc1a4b02bbe34e41d71
Gurkamal Deol <gdeol4@uwo.ca>
2022-07-15 13:24:34 -0400

    adding resources

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title('Bioinformatics POC resources')
+
+st.header('Python basics')
+
+st.markdown['Intro to python for data science']('https://www.datacamp.com/courses/intro-to-python-for-data-science')
+st.markdown['Introduction to Python']('https://realpython.com/learning-paths/python3-introduction/')
+st.markdown['Basic Data Types in Python']('https://realpython.com/python-data-types/')
+
+st.header('Pandas basics')
+st.markdown['Data Cleaning With pandas and NumPy']('https://realpython.com/courses/data-cleaning-with-pandas-and-numpy/')
+st.markdown['DataFrames in Python']('https://www.datacamp.com/tutorial/pandas-tutorial-dataframe-python')
+st.markdown['How to Learn pandas']('https://www.datacamp.com/blog/how-to-learn-pandas')
+st.markdown['Pandas for Beginners']('https://www.datacamp.com/tutorial/pandas')
+st.markdown['Pandas read csv(): Importing Data']('https://www.datacamp.com/tutorial/pandas-read-csv')
+
+
+st.header('More resources')
+st.markdown['Pandas Cheat Sheet: Data Wrangling in Python']('https://www.datacamp.com/cheat-sheet/pandas-cheat-sheet-data-wrangling-in-python')
+st.markdown['Jupyter Notebook Tutorial: The Definitive Guide']('https://www.datacamp.com/tutorial/tutorial-jupyter-notebook')
+st.markdown['Git Cheat Sheet']('https://www.datacamp.com/cheat-sheet/git-cheat-sheet')
+#st.markdown['']('')
